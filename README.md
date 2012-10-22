e7ccf26336e1ee93e73824bbf0df54375ddd5a57
Mark Steadman <marksteadman@me.com>
2012-10-22 12:41:36 +0100

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-jquery.tweetspan
+jQuery.tweetspan
 ================
 
-Pull updates from a Twitter account straight into an HTML element
+Pull updates from a Twitter account straight into an HTML element. Check out test.html for a simple example.
+
+You can use any field from the
+[https://dev.twitter.com/docs/api/1/get/search](Twitter Search JSON API)'s result set. You can use filters
+to determine how text should be formatted. Currently available filters are:
+
+- ``tweet``: To format tweet contents, linking @ usernames, hashtags and URLs
+- ``localtime``: To convert the ``created_at`` field into a local time string
+- ``timesince``: To convert the ``created_at`` field into a string showing how long ago the tweet was posted
+- ``capfirst``: To capitalise the first letter of a string.
+
+To chain filters together, separate each filter name with a space, just as you would with classes.
+
+Any questions, please contact [http://substrakt.co.uk](Substrakt).
---
+++ jquery.tweetspan.js
@@ -0,0 +1,142 @@
+jQuery(document).ready(
+	function($) {
+		var twitterFilters = {
+			tweet: function(text) {
+				var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i;
+				text = text.replace(exp, "<a href='$1' target='_blank'>$1</a>");
+				
+				exp = /(^|\s)#(\w+)/g;
+				text = text.replace(exp, "$1<a href='http://search.twitter.com/search?q=%23$2' target='_blank'>#$2</a>");
+				
+				exp = /(^|\s)@(\w+)/g;
+				text = text.replace(exp, "$1<a href='http://www.twitter.com/$2' target='_blank'>@$2</a>");
+				
+				return text;
+			},
+			localtime: function(text) {
+				return new Date(Date.parse(text)).toLocaleString();
+			},
+			timesince: function(text) {
+				var tTime = new Date(text);
+				var cTime = new Date();
+				var sinceMin = Math.round((cTime - tTime) / 60000);
+
+				if(sinceMin == 0) {
+					var sinceSec = Math.round((cTime - tTime) / 1000);
+					if(sinceSec < 10) {
+						var since = 'less than 10 seconds ago';
+					} else if(sinceSec < 20) {
+						var since = 'less than 20 seconds ago';
+					} else {
+						var since = 'half a minute ago';
+					}
+				} else if(sinceMin == 1) {
+					var sinceSec=Math.round((cTime - tTime) / 1000);
+					if(sinceSec == 30) {
+						var since = 'half a minute ago';
+					} else if(sinceSec < 60) {
+						var since = 'less than a minute ago';
+					} else {
+						var since = 'a minute ago';
+					}
+				} else if(sinceMin < 45) {
+					var since = sinceMin +' minutes ago';
+				} else if(sinceMin > 44 && sinceMin < 60) {
+					var since = 'about an hour ago';
+				} else if(sinceMin < 1440) {
+					var sinceHr = Math.round(sinceMin / 60);
+					if(sinceHr == 1) {
+						var since = 'about an hour ago';
+					} else {
+						var since = 'about ' + sinceHr + ' hours ago';
+					}
+				} else if(sinceMin > 1439 && sinceMin < 2880) {
+					var since = 'a day ago';
+				} else {
+					var sinceDay = Math.round(sinceMin / 1440);
+					var since = sinceDay + ' days ago';
+				}
+
+				return since;
+			},
+			capfirst: function(text) {
+				return text.substr(0, 1).toUpperCase() + text.substr(1);
+			}
+		}
+		
+		function formatTweetFields(jsonParent, htmlParent, parentKey) {
+			var kk, field, text, filters, filter;
+			
+			for(var k in jsonParent) {
+				text = jsonParent[k];
+				if(typeof text == 'object') {
+					formatTweetFields(text, htmlParent, k);
+					continue;
+				}
+				
+				if(typeof parentKey != 'undefined') {
+					kk = parentKey + '.' + k;
+				} else {
+					kk = k;
+				}
+				
+				field = htmlParent.find('[data-field="' + kk + '"]');
+				if(field.length == 0) {
+					continue;
+				}
+				
+				filters = field.attr('data-format');
+				
+				if(filters) {
+					filters = filters.split(' ');
+					for(var f = 0; f < filters.length; f ++) {
+						filter = filters[f];
+						
+						if(filter) {
+							filter = twitterFilters[filter];
+							if(typeof filter != 'undefined') {
+								text = filter(text);
+							}
+						}
+					}
+				}
+				
+				if(field[0].tagName.toLowerCase() != 'img') {
+					field.html(text);
+				} else {
+					field.attr('src', text);
+				}
+			}
+		}
+		
+		$('.tweets[data-account]').each(
+			function() {
+				var self = $(this);
+				var account = self.attr('data-account');
+				var count = parseInt(self.attr('data-count'));
+				var url = 'http://search.twitter.com/search.json?from=' + account + '&rpp=' + count
+				
+				$.getJSON(url + '&callback=?',
+					(
+						function(context) {
+							return function(data) {
+								var template = context.find('.tweet');
+								var results = typeof data.results != 'undefined' ? data.results : [];
+								var tweet;
+								
+								for(var i = 0; i < results.length; i ++) {
+									tweet = template.clone();
+									formatTweetFields(results[i], tweet);
+									context.append(tweet);
+								}
+								
+								template.remove();
+								context.show();
+							}
+						}
+					)(self)
+				);
+			}
+		).hide();
+	}
+);
